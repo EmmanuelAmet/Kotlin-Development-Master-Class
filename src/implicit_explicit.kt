0ef7fd0446d5deaed2cbe859daae447e230604a1
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-07-30 03:24:51 -0700

    Implicit and Explicit Declaration

---
+++ src/implicit_explicit.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val price = 3
+    println(price::class.java)
+    val item = 354
+    println(item::class.java)
+    val cats: Byte = 5
+    println(cats::class.java)
+    val life: Long = 34
+    println(life::class.java)
+    val newlife = 34L
+    println(newlife::class.java)
+    val f = 34.5F
+    println(f::class.java)
+}
