e893db09b143b6e02734caeb26f2e5f575405646
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-07-29 23:40:56 -0700

    Variables

---
+++ src/variables.kt
@@ -0,0 +1,30 @@
+fun main() {
+    //Variables
+     var  tvShows = "Many"
+    println(tvShows)
+    tvShows = "Too Many"
+    println(tvShows)
+    /*
+    Types of variable
+    1. Mutable
+    2. Immutable
+     */
+
+    val color = "Blue"
+    //color = "Red" Error
+    val name = "My cat name is \"Fluffy\""
+    println(name)
+
+    val slash = "Two types of slashes \\ and /"
+    println(slash)
+
+    val url = "http://google.com"
+    println(url)
+
+    val lineOne = "(\\(\\"
+    val lineTwo = "(-.-)"
+    val lineThree = "o_(\")(\")"
+    println(lineOne)
+    println(lineTwo)
+    println(lineThree)
+}
