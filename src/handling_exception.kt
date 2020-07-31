14e09d2114651ef99b6e382ef6a7d212656776de
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-07-30 22:05:19 -0700

    Introduction to Exception Handling

---
+++ src/handling_exception.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val test = readLine()
+    println(test?.toInt())
+    /*
+    "C:\Program Files\Java\jdk-12.0.2\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2018.3.1\lib\idea_rt.jar=58881:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2018.3.1\bin" -Dfile.encoding=UTF-8 -classpath "D:\All Projects\Kotlin Course\KotlinMasterClass\out\production\KotlinMasterClass;C:\Users\EMMANUEL\.IdeaIC2018.3\config\plugins\Kotlin\kotlinc\lib\kotlin-stdlib.jar;C:\Users\EMMANUEL\.IdeaIC2018.3\config\plugins\Kotlin\kotlinc\lib\kotlin-reflect.jar;C:\Users\EMMANUEL\.IdeaIC2018.3\config\plugins\Kotlin\kotlinc\lib\kotlin-test.jar;C:\Users\EMMANUEL\.IdeaIC2018.3\config\plugins\Kotlin\kotlinc\lib\kotlin-stdlib-jdk7.jar;C:\Users\EMMANUEL\.IdeaIC2018.3\config\plugins\Kotlin\kotlinc\lib\kotlin-stdlib-jdk8.jar" Handling_exceptionKt
+rsg
+Exception in thread "main" java.lang.NumberFormatException: For input string: "rsg"
+	at java.base/java.lang.NumberFormatException.forInputString(NumberFormatException.java:68)
+	at java.base/java.lang.Integer.parseInt(Integer.java:658)
+	at java.base/java.lang.Integer.parseInt(Integer.java:776)
+	at Handling_exceptionKt.main(handling_exception.kt:3)
+	at Handling_exceptionKt.main(handling_exception.kt)
+
+Process finished with exit code 1
+     */
+}
