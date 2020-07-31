301d298708a15e5a4cb378b080385b2a7c317072
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-07-31 03:41:47 -0700

    Range

---
+++ src/range.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Enter a number")
+    val input = readLine()?:"0"
+    val number = input.toInt()
+    if(number !in 0..9){
+        println("Your number is not a single digit")
+    }
+}
