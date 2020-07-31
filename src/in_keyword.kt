215c52c46457eb80909f3ce498c956cec35ef696
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-07-31 03:34:01 -0700

    In Keyword

---
+++ src/in_keyword.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val favouritePet = "dog"
+    val availablePets = listOf("dog", "cat", "horse")
+    if(favouritePet in availablePets){
+        println("We have your next best pet")
+    }else{
+        println("Sorry, $favouritePet is not available")
+    }
+
+    if(favouritePet !in availablePets){
+        println("Not available")
+    }else{
+        println("Sorry, $favouritePet available")
+    }
+}
