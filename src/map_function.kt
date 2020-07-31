0c1e7888719ba57a656a5eacc1d27bb3f26a7dfa
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-07-31 01:31:38 -0700

    Map Functions

---
+++ src/map_function.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val count = mapOf(Pair(1, "One"), Pair(2, "Two"), Pair(3, "Three"))
+    println("Size $count.size")
+    println("Contains ${count.containsKey(2)}")
+    println("contain Value: ${count.containsValue("Th")}")
+    println("contain Value say One: ${count.containsValue("One")}")
+    println(count.isEmpty())
+    println(count.isNotEmpty())
+}
