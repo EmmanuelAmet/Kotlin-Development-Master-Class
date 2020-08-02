e7ff3261ffe0d18a9e82f62a673fcd7c66ed8680
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-08-02 11:55:44 -0700

    More on Class and Object

---
+++ src/OOP/classes_objects.kt
@@ -0,0 +1,24 @@
+package OOP
+
+fun main() {
+    //val animals = listOf("Cat", "Dog", "Horse")
+    val animal:ArrayList<String> = arrayListOf()
+    val table = Table()
+    println(table.color)
+    val myGarage = Garage()
+    myGarage.car
+}
+
+class Table{
+    var color = "white"
+}
+
+class MyCar{
+    fun drive(){
+        println("Driving along in my car")
+    }
+}
+
+class Garage{
+    val car = MyCar()
+}
