a39f874d6f50dbb2e7aecfc826616a3ee590580f
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-08-02 11:45:59 -0700

    Object Construct -OOP

---
+++ src/OOP/object_construct.kt
@@ -0,0 +1,23 @@
+package OOP
+
+fun main() {
+    DatabaseAccess.connected = true
+    if(DatabaseAccess.connected){
+        DatabaseAccess.disconnected()
+    }else{
+        DatabaseAccess.connect()
+    }
+    println("Database is connected: ${DatabaseAccess.connected}")
+}
+
+object DatabaseAccess{
+    var connected = false
+    fun connect(){
+        connected = true
+        println("DB Connected.")
+    }
+    fun disconnected(){
+        connected = false
+        println("Disconnected from the DB")
+    }
+}
