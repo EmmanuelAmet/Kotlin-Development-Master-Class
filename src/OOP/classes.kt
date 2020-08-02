498e96458783306f0773294ffea80dbf553727df
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-08-02 06:49:49 -0700

    Introduction Object Oriented Programming - Classes

---
+++ src/OOP/classes.kt
@@ -0,0 +1,17 @@
+package OOP
+
+fun main() {
+
+}
+class Car{
+    var model:String? = null
+    val topSpeed = 100
+
+    fun start(){
+        println("Starting the $model")
+    }
+
+    fun drive(speed:Int){
+        println("Driving at a speed of $speed")
+    }
+}
