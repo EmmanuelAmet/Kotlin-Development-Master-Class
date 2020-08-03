f0fae2713dc87dd53776fffd59ae07ce34fce648
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-08-02 22:02:27 -0700

    Init Block

---
+++ src/oop/init_block.kt
@@ -0,0 +1,28 @@
+package oop
+
+fun main() {
+    var theCar = TheCar()
+    val os = OS("Kali")
+    val webBrowser = WebBrowser()
+}
+
+class WebBrowser(){
+    init {
+        println("Web browser is connecting...")
+        println("loading http://www.google.com/")
+    }
+}
+
+class OS(operatingSystem:String){
+    init {
+        println("OS $operatingSystem is installing, please wait.")
+    }
+}
+
+class TheCar{
+    var module:String = ""
+    var topSpeed = 100
+    init {
+        println("This car is a model $module and has a top speed of $topSpeed")
+    }
+}
