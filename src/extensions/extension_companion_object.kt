e6fc59afca6342ff73e855fddfcdf6e0ef9cfcaf
Emmanuel Ametepee <emmanuelamet11@gmail.com>
2020-08-06 04:23:02 -0700

    Extension Companion object

---
+++ src/extensions/extension_companion_object.kt
@@ -0,0 +1,12 @@
+package extensions
+
+fun main() {
+    Book.printMe()
+}
+
+class Book{
+    companion object{}
+}
+fun Book.Companion.printMe(){
+    println("Car companion object")
+}
--- src/extensions/extension_functions.kt
+++ src/extensions/extension_functions.kt
@@ -3,8 +3,15 @@ package extensions
 fun main() {
     val name = "Emmanuel"
     println(name.slim())
+    println("Grace".getCustomName())
+    println(3.getCustomName())
+    println(2.5F.getCustomName())
 }
 
 fun String.slim() :String{
      return this.substring(1, length-1)
 }
+
+fun String.getCustomName() = "A string of characters"
+fun Int.getCustomName() = "An integer Number"
+fun Float.getCustomName() = "A floating point Number"
